063d8899730d5507c8319e8cd456db7b3d6fba0d
Stephen Gachoka <stephengachoka57@gmail.com>
2022-07-06 14:18:23 +0300

    simple unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Should render logo on start', () => {
+test('Should render logo image on start', () => {
   render(<App />);
   const logo = screen.getByAltText('logo');
   expect(logo).toBeInTheDocument();
